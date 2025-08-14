e6a951aad43192dea533eb52147fab3cd7a9f51a
chaebinn <juchaebin975@gmail.com>
2025-08-15 08:14:42 +0900

    버튼 일부 수정

--- src/pages/Market/MarketPage.jsx
+++ src/pages/Market/MarketPage.jsx
@@ -2,48 +2,19 @@ import React, { useState } from "react";
 import styles from "./MarketPage.module.css";
 import ProductList from "./components/ProductList.jsx";
 
-export default function MarketPage({ label, active, onClick }) {
-  const [activeSort, setActiveSort] = useState(0);
+export default function MarketPage() {
   return (
     <>
       <div className={styles.topBar}>
         <div className={styles.count}>판매 상품 31개</div>
         <div className={styles.buttonContainer}>
-          <button
-            className={`${styles.button} ${active ? styles.active : ""}`}
-            onClick={onClick}
-            type="button"
-          >
-            최신순
-          </button>
-          <button
-            className={`${styles.button} ${active ? styles.active : ""}`}
-            onClick={onClick}
-            type="button"
-          >
-            인기순
-          </button>
-          <button
-            className={`${styles.button} ${active ? styles.active : ""}`}
-            onClick={onClick}
-            type="button"
-          >
-            저가순
-          </button>
-          <button
-            className={`${styles.button} ${active ? styles.active : ""}`}
-            onClick={onClick}
-            type="button"
-          >
-            신선순
-          </button>
+         <button className={styles.button} type="button">최신순</button>
+          <button className={styles.button} type="button">인기순</button>
+          <button className={styles.button} type="button">저가순</button>
+          <button className={styles.button} type="button">신선순</button>
         </div>
       </div>
-      {/* 상품 목록 */}
       <ProductList />
-
-      {/* <SortButton label="최신순" active={true} onClick={() => alert("클릭됨")} />
-      <SortButton label="인기순" active={false} onClick={() => alert("클릭됨")} /> */}
     </>
   );
 }
