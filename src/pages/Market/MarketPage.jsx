b66897c084d31533cb846728c7a1e591234ba394
chaebinn <juchaebin975@gmail.com>
2025-08-17 01:09:25 +0900

    feat: 상품목록 API 연결 및 환경변수 설정

--- src/pages/Market/MarketPage.jsx
+++ src/pages/Market/MarketPage.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import styles from "./MarketPage.module.css";
 import ProductList from "./components/ProductList.jsx";
+import axios from "axios";
 
 export default function MarketPage() {
+  const { merchantId } = useParams(); //URL에서 상점 ID 추출
+  const [sort, setSort] = useState("recent"); //기본값 최신순
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/api/products/merchants/${merchantId}`, { params: { sort } })
+      .then((res) => setProducts(res.data))
+      .catch(console.error);
+  }, [merchantId, sort]);
+
+  const count=products.length; 
+
   return (
     <>
       <div className={styles.topBar}>
-        <div className={styles.count}>판매 상품 31개</div>
+        <div className={styles.count}>판매 상품 {count}개</div>
         <div className={styles.buttonContainer}>
-         <button className={styles.button} type="button">최신순</button>
-          <button className={styles.button} type="button">인기순</button>
-          <button className={styles.button} type="button">저가순</button>
-          <button className={styles.button} type="button">신선순</button>
+          {["recent","popular","cheap","fresh"].map(key => (
+            <button
+              key={key}
+              className={`${styles.button} ${sort===key ? styles.active : ""}`}
+              type="button"
+              onClick={() => setSort(key)}
+            >
+              {key==="recent"?"최신순":key==="popular"?"인기순":key==="cheap"?"저가순":"신선순"}
+            </button>
+          ))}
         </div>
       </div>
-      <ProductList />
+      <ProductList products={products}/>
     </>
   );
 }
--- src/pages/Market/components/ProductList.jsx
+++ src/pages/Market/components/ProductList.jsx
@@ -1,7 +1,9 @@
 import { products } from "../product.js";
+//임시 더미데이터(상품)
 import styles from "../MarketPage.module.css";
 
-function ProductList() {
+// props에서 products 배열을 받아서 사용
+function ProductList() { 
   return (
     <div className={styles.cardSection}>
       {products.map((p) => (
@@ -11,10 +13,10 @@ function ProductList() {
             // 이미지 삽입 전
             style={{ backgroundImage: `url(${p.imageUrl})` }}
           >
-            <button className={styles.cartButton}></button>
+            <button className={styles.cartButton} onClick={()=> console.log(`${p.name} 장바구니 추가`)}></button>
           </div>
           <div className={styles.cardName}>{p.name}</div>
-          <div className={styles.cardPrice}>{p.price.toLocaleString()}원</div>
+          <div className={styles.cardPrice}>{p.price.toLocaleString()}원</div> 
           <div className={styles.cardDate}>{p.endDate}까지</div>
         </div>
       ))}
