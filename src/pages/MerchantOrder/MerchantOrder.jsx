0c42ef148eda0e3ac0e697ee461afcc9248961b4
chaebinn <juchaebin975@gmail.com>
2025-08-23 14:51:14 +0900

    오타 수정

--- src/pages/MerchantOrder/MerchantOrder.jsx
+++ src/pages/MerchantOrder/MerchantOrder.jsx
@@ -5,7 +5,7 @@ import Header from "../../components/Header";
 function MerchantOrder() {
   return (
     <div>
-      <Header label="주문/픽업" to="-1"/>
+      <Header label="주문/픽업" />
       주문/픽업 페이지
       <MerchantMenuBar />
     </div>
